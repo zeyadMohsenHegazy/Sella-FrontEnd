41554381ef9b27ba4ca64c3194d400541f84aea2
Zeyad_Mohsen <zeyadmohsen1999@gmail.com>
2023-07-05 18:10:34 +0300

    Create Not-Found and customize it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './Components/Footer/footer.component';
 import { HeaderComponent } from './Components/header/header.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
