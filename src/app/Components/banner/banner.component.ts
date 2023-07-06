57930cacd6b01f6278677ce50717b93b6fcc3e30
Zeyad_Mohsen <zeyadmohsen1999@gmail.com>
2023-07-07 01:56:26 +0300

    banner section Added and customize bootstrap js

--- src/app/Components/banner/banner.component.ts
+++ src/app/Components/banner/banner.component.ts
@@ -12,22 +12,6 @@ export class BannerComponent implements OnInit{
   constructor() { }
 
   ngOnInit(): void {
-    // Add the owlCarousel initialization code here
-    $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      responsive:{
-        0:{
-          items:1
-        },
-        600:{
-          items:3
-        },
-        1000:{
-          items:3
-        }
-      }
-    });
+  
   }
 }
