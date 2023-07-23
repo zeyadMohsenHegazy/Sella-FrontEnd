07197f9809b6278d4733d9ad49443d90c5f40fa9
Zeyad Mohsen <127048807+zeyadMohsenHegazy@users.noreply.github.com>
2023-07-23 06:10:39 +0300

    Error 404 page UI Customization

--- src/app/Components/not-found/not-found.component.ts
+++ src/app/Components/not-found/not-found.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare class Parallax {
+  constructor(element: HTMLElement);
+}
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
+  scene!: HTMLElement | null;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.scene = document.getElementById('scene');
+    if (this.scene) {
+      let parallax: Parallax = new Parallax(this.scene);
+    }
+  }
 }
