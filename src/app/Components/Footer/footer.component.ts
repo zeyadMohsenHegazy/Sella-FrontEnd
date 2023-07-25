63f6cf3e1f48f5defa1afc0a5564fccce5d06f6b
Zeyad Mohsen <127048807+zeyadMohsenHegazy@users.noreply.github.com>
2023-07-25 07:29:59 +0300

    UI Angular Customization

--- src/app/Components/Footer/footer.component.ts
+++ src/app/Components/Footer/footer.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  openFacebook(): void {
+    window.open('https://www.facebook.com', '_blank');
+  }
+
+  openTwitter(): void{
+    window.open('https://twitter.com', '_blank');
+  }
+
+  openYoutube(): void{
+    window.open('https://www.youtube.com', '_blank');
+  }
+
+  openInsta(): void{
+    window.open('https://www.instagram.com', '_blank');
+  }
+
+  openGithub(): void{
+    window.open('https://github.com/zeyadMohsenHegazy?tab=repositories', '_blank');
+  }
 }
